8811f65d64e7000a697013abc73a5c8d6f2a4aee
Gerry1Laxy <mr.john.milton@yandex.ru>
2022-06-27 16:29:40 +0300

    complete yandex disk task

---
+++ yandex_disk/main.py
@@ -0,0 +1,23 @@
+import yadisk
+import os
+import my_token
+
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def upload(self, file_path: str) -> None:
+        disk = yadisk.YaDisk(token=self.token)
+        if os.path.exists(file_path):
+            res = disk.upload(file_path, file_path.split('/')[-1])
+            print(res)
+        else:
+            print('No such file')
+
+
+if __name__ == '__main__':
+    work_dir = os.getcwd()
+    path_to_file = os.path.join(work_dir, 'files/yad.txt')
+    uploader = YaUploader(my_token.token)
+    uploader.upload(path_to_file)
